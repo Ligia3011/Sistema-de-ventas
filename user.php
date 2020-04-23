232ee5319e5c9ec14e1000136e559b97ab8d8300
Marcia Abigáil Mejía Reyes <201900027@colsanjose.edu.gt>
2020-04-23 16:49:00 -0600

    :'(

---
+++ user.php
@@ -0,0 +1,29 @@
+<?php
+	$usuario=array("Ligia","Abby");
+	$pass=array(123,456);
+
+	$estado=false;
+
+	$nombre=$_POST['nombre'];
+	$password=$_POST['pass']
+
+	$tam=count($usuario);
+
+	for($x=0; $x<$tam; $x++)
+	{
+	 if($usuario[$x]==$nombre && $pas[$x]==$password)
+	 {
+	 $estado=true;
+	 }
+	}
+	if($estado)
+	{
+	echo "user";
+	}
+	else 
+	{
+		echo "no user";
+	}
+}
+
+?>
