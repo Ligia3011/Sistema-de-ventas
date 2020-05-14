30f48a3d694b313718630f44823a93505251edd9
Ligia3011 <62628152+Ligia3011@users.noreply.github.com>
2020-05-13 19:41:15 -0600

    conex

---
+++ Conexion.php
@@ -0,0 +1,14 @@
+<? php
+     Conexion  clase extiende mysqli
+    {
+	    función  pública __construct ()
+       {
+
+	        parent :: __construct ( 'localhost' , 'root' , '' , 'Usuarios' );
+            $ this -> query ( "SET NAMES 'utf8';" );
+            $ this -> connect_errno ? die ( 'Error en la conexión de la base de datos.' ): $ error = "<br> <br> Conectado con la base de datos:" ;
+            // echo $ error;
+            desarmado ( $ error );
+       }
+    }
+?>
